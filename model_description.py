845dafa6e4ee47a5441af8cd4d54aba018456ca1
miriamd3 <116340656+miriamd3@users.noreply.github.com>
2022-11-15 12:33:17 +0100

    wip_miri

--- model_description.py
+++ model_description.py
@@ -17,35 +17,26 @@ run_parameter= run_parameter(scenario_name = "Offshore_Bidding_Zone_Scenario")
 run_parameter.create_scenarios()
 
 data = model_data(create_res = False,reduced_ts = True, export_files= True, run_parameter = run_parameter)
-data.nodes.head()
-gdf_nodes = gpd.data.nodes(data.nodes, geometry=gpd.points_from_xy(data.nodes.LON, data.nodes.LAT), crs="EPSG:4326")
-
+df_nodes = data.nodes
+gdf_nodes = gpd.df_nodes(df_nodes, geometry=gpd.points_from_xy(df_nodes.LON, df_nodes.LAT), crs="EPSG:4326")
+#TODO: error module 'geopandas' has no attribute 'df_nodes'? Warum?
 
 #zones festlegen, als set und zuordnung zu den nodes
 shapes = gpd.read_file('data/shapes/NUTS_RG_10M_2021_4326.geojson')
 shapes_filtered = shapes.query("LEVL_CODE ==1 and CNTR_CODE == 'DE'")
-shapes_filtered.plot()
-
+#shape from the nodes longitude and latitude a geometry to sjoin them
 df_buses = pd.read_csv("data/PyPSA_elec1024/buses.csv", index_col=0)
 #df_buses['geometry'] = [Point(xy) for xy in zip(df_buses.x, df_buses.y)]
 gdf_buses = gpd.GeoDataFrame(df_buses, geometry=gpd.points_from_xy(df_buses.x, df_buses.y), crs="EPSG:4326")
-#df_buses_selected = ['name', 'geometry']
-df_buses.head()
 # coordinate systems are correct?
 #df_buses_selected.crs == shapes_filtered.crs
-#Spatial Join
+#Spatial Join (sjoin from geopandas)
 #sjoined_nodes_states = gpd.sjoin(df_buses["geometry"],shapes_filtered, op="within")
 sjoined_nodes_states = gdf_buses.sjoin(shapes_filtered[["NUTS_NAME","NUTS_ID","geometry"]], how ="left")
-
-#How many nodes are in each state bzw zone "state_Bayern" = "NUTS_ID":"DE2"
-# First grouping based on "NUTS_ID" - Within each team we are grouping based on "Position"
-df_nodes_Bayern = sjoined_nodes_states.groupby("NUTS_ID").count()
-#ToDo
-#größte Aufteilung der Zonen festlegen
-
 #Filtern der Columns die wir brauchen für Zones DE
 df_zones_DE = sjoined_nodes_states.query("country == 'DE'")
 df_zones_DE_filtered = df_zones_DE.filter(['NUTS_NAME', 'country', 'NUTS_ID', 'geometry'])
+
 #How many nodes are in each state bzw zone "state_Bayern" = "NUTS_ID":"DE2"?
 df_zones_DE_filtered.groupby("NUTS_NAME == 'Bayern'").count()
 
